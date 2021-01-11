7685e76f5ff97f0dd47455e1e5887f7c66cefc4f
ademir <60299587+alidemirbay@users.noreply.github.com>
2021-01-11 13:54:46 -0800

    added get, post, put routes

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const router = express.Router();
+const burger = require("../models/burger.js");
+
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObj = { burgers: data };
+        res.render("index", hbsObj);
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne("burger_name", req.body.burger_name, function (result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    burger.updateOne(req.params.id, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else { res.status(200).end(); }
+    });
+});
+
+module.exports = router;

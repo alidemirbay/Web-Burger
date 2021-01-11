632cc80f2fb9ec340d07f432c9dbfa0c14feda57
ademir <60299587+alidemirbay@users.noreply.github.com>
2021-01-11 15:56:53 -0800

    added js code for devour it button

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -12,18 +12,19 @@ $(function () {
             data: newBurger
         }).then(function () {
             location.reload();
-        }
-        );
+        });
     });
 
-
-
-
-
-
-
-
-
-
-
-}
+    $(".change-status").on("click", function (event) {
+        var id = $(this).data("id");
+        var devoured = $(this).data("devoured");
+        var newState = { devoured: devoured };
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newState
+        }).then(function () {
+            console.log("changed status to", devoured);
+            location.reload();
+        });
+    });
+});
